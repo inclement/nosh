4c1cfdbd30ae9d40ea63584a67c7d8e0043d81f6
Alexander Taylor <alexanderjohntaylor@gmail.com>
2016-09-20 21:20:21 +0100

    Added new dirutils.py file

--- nosh/__init__.py
+++ nosh/__init__.py
@@ -1,4 +1,4 @@
 
 from nosh.shell import (mv, rm, cp, pwd, ls, mkdir, touch)
-from nosh.utils import current_directory
+from nosh.dirutils import current_directory, temp_directory
 from nosh.archives import (tar, zip, unzip)
---
+++ nosh/dirutils.py
@@ -0,0 +1,27 @@
+import contextlib
+import os
+import shutil
+import tempfile
+
+@contextlib.contextmanager
+def current_directory(new_dir):
+    '''Context manager to temporarily move to a different directory.
+
+    '''
+    new_dir = expand_path(new_dir)
+    cur_dir = os.getcwd()
+    os.chdir(new_dir)
+    yield
+    os.chdir(cur_dir)
+
+@contextlib.contextmanager
+def temp_directory():
+    '''Context manager returning a temporary directory name.  This
+    directory, and all its contents, is deleted at the end of the
+    context.
+
+    '''
+    temp_dir = tempfile.mkdtemp()
+    yield temp_dir
+    shutil.rmtree(temp_dir)
+
